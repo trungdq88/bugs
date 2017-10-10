51803fc2fa5be9fc43373b96c15c0f5c7a258e95
Trung Dinh Quang <trungdq88@gmail.com>
2017-10-10 19:33:46 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ const BUG_POPULATION = 10;
 const FOOD_SIZE = 5;
 const BUG_SIZE = 5;
 const BUG_SPEED = 2;
+const BUG_MAX_AGE = 200;
+const BUG_BONUS_AGE = 200;
 
 const sketch = p => {
   class Food {
     position = p.createVector(0, 0);
+    gone = false;
     constructor(position) {
       this.position = position;
     }
@@ -16,15 +19,29 @@ const sketch = p => {
       p.fill(0);
       p.ellipse(this.position.x, this.position.y, FOOD_SIZE);
     };
+    eaten = () => {
+      this.gone = true;
+    };
   }
   class Bug {
     brain = {};
     angle = p.random(0, p.TWO_PI); // radian
     position = p.createVector(0, 0);
     dead = false;
-    constructor(position) {
+    age = 0;
+    constructor(position, foods = []) {
       this.position = position;
     }
+    getClosestFood = () => {
+      return foods
+        .map((food, index) => ({
+          ...food,
+          distance: p5.Vector.sub(food.position, this.position).mag(),
+        }))
+        .reduce((prev, curr) => {
+          return prev && prev.distance < curr.distance ? prev : curr;
+        }, null);
+    };
     draw = () => {
       p.fill(255);
       p.line(
@@ -33,6 +50,18 @@ const sketch = p => {
         this.position.x - Math.cos(this.angle) * 10,
         this.position.y - Math.sin(this.angle) * 10,
       );
+      p.line(
+        this.position.x,
+        this.position.y,
+        this.position.x + Math.cos(this.angle + p.QUARTER_PI) * 8,
+        this.position.y + Math.sin(this.angle + p.QUARTER_PI) * 8,
+      );
+      p.line(
+        this.position.x,
+        this.position.y,
+        this.position.x + Math.cos(this.angle - p.QUARTER_PI) * 8,
+        this.position.y + Math.sin(this.angle - p.QUARTER_PI) * 8,
+      );
       p.ellipse(this.position.x, this.position.y, BUG_SIZE);
     };
     update = () => {
@@ -40,10 +69,17 @@ const sketch = p => {
         this.position.x < 0 ||
         this.position.x > SIZE ||
         this.position.y < 0 ||
-        this.position.y > SIZE
+        this.position.y > SIZE ||
+        this.age > BUG_MAX_AGE
       ) {
         this.dead = true;
         return;
+      }
+      this.age += 1;
+      const closestFood = this.getClosestFood();
+      if (closestFood && closestFood.distance < (FOOD_SIZE + BUG_SIZE) / 2) {
+        this.age -= BUG_BONUS_AGE;
+        closestFood.eaten();
       }
       this.position.add(
         p.createVector(
@@ -59,7 +95,11 @@ const sketch = p => {
 
   setInterval(() => {
     foods.push(new Food(p.createVector(p.random(SIZE), p.random(SIZE))));
-  }, 1000);
+  }, 100);
+
+  setInterval(() => {
+    bugs.forEach((bug, index) => (bugs[index].angle = p.random(0, p.TWO_PI)));
+  }, 500);
 
   const createBugs = () => {
     bugs = [];
@@ -76,7 +116,7 @@ const sketch = p => {
 
   p.draw = function() {
     p.background(200);
-    foods.forEach(food => food.draw());
+    foods.filter(food => !food.gone).forEach(food => food.draw());
     bugs.filter(bug => !bug.dead).forEach(bug => {
       bug.update();
       bug.draw();
